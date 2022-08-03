c1c006473998c67f0107e1d3d6431999c11457d1
ernanimusa <ernani@musa.co>
2022-08-03 13:42:16 -0300

    add mais um campo na estrutura do GO e no banco

--- models/personalidades.go
+++ models/personalidades.go
@@ -4,6 +4,7 @@ type Personalidade struct {
 	Id       int    `json:"id"`
 	Nome     string `json:"nome"`
 	Historia string `json:"historia"`
+	Obs      string `json:"observacao"`
 }
 
 var Personalidades []Personalidade
